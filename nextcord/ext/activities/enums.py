2e4dd73fa31fedc0564d7fee35045e917cf83b1b
Minh Dương <i-am@maskduck.ninja>
2022-02-25 18:18:07 +0700

    add boost locked notice

--- nextcord/ext/activities/enums.py
+++ nextcord/ext/activities/enums.py
@@ -2,15 +2,15 @@ from enum import Enum
 
 
 class Activity(Enum):
-    poker = 1
+    poker = 1  # boost-locked
     betrayal = 2
     fishington = 3
-    chess = 4
-    checker = 5
-    ocho = 6
+    chess = 4  # boost-locked
+    checker = 5  # boost-locked
+    ocho = 6  # boost-locked
     youtube = 7
     doodle = 8
-    letter_tile = 9
+    letter_tile = 9  # boost-locked, now named letter_league
     word_snacks = 10
     sketch = 11
-    spellcast = 12
+    spellcast = 12  # boost-locked
