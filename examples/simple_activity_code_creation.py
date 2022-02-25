71fbbfebf4bec832cd2ceac07e336040bb9cbfe9
MaskDuck <i-am@maskduck.ninja>
2022-02-25 11:47:54 +0700

    Create simple_activity_code_creation.py

---
+++ examples/simple_activity_code_creation.py
@@ -0,0 +1,22 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord.ext import activities
+
+intents = nextcord.Intents.default()
+bot = commands.Bot(command_prefix = "$")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {str(bot.user)} (ID {bot.user.id})")
+
+@bot.command()
+async def poker(ctx, channel: nextcord.VoiceChannel):
+    invite_link = await channel.create_activity_invite(activities.Activity.poker)
+    await ctx.send(invite_link)
+
+@bot.command()
+async def betrayal(ctx, channel: nextcord.VoiceChannel):
+    invite_link = await channel.create_activity_invite(activities.Activity.betrayal)
+    await ctx.send(invite_link)
+
+bot.run('token here')
