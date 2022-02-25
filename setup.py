b768f2ca729ba2fac18de59ab6351aa34691f5db
Minh Dương <i-am@maskduck.ninja>
2022-02-25 10:52:46 +0700

    urgent fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 classifiers = [
   'Development Status :: 1 - Planning',
-  'Intended Audience :: Education',
+  'Intended Audience :: Developers',
   'Operating System :: Microsoft :: Windows :: Windows 10',
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3'
