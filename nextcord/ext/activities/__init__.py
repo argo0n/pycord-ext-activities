3b11f4ec28e977e9831e40b52ac9e41e1883499e
Minh Dương <i-am@maskduck.ninja>
2022-03-04 11:30:46 +0700

    no typeguard pls

--- nextcord/ext/activities/__init__.py
+++ nextcord/ext/activities/__init__.py
@@ -10,7 +10,7 @@ __author__ = "MaskDuck"
 __license__ = "GNU General Public License Version 3.0"
 
 
-async def create_activity_invite_link(self, activity: Activity, activity_id: int = None) -> TypeGuard[Union[str, None]]:  # noqa: E501
+async def create_activity_invite_link(self, activity: Activity, activity_id: int = None) -> str:  # noqa: E501
     """
     Creates an invite link for the specified activity.
 
