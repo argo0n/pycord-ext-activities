392ecb22593b5563f6e1124183e97574d3841e20
Minh Dương <i-am@maskduck.ninja>
2022-03-04 11:30:12 +0700

    fix

--- nextcord/ext/activities/__init__.py
+++ nextcord/ext/activities/__init__.py
@@ -2,14 +2,15 @@ import nextcord
 from .enums import Activity
 
 import warnings
+from typing import TypeGuard, Union
 
 __all__ = ("Activity")
-__version__ = "2022.02.26"
+__version__ = "1.0.0a1"
 __author__ = "MaskDuck"
 __license__ = "GNU General Public License Version 3.0"
 
 
-async def create_activity_invite_link(self, activity: Activity, activity_id: int = None) -> str:  # noqa: E501
+async def create_activity_invite_link(self, activity: Activity, activity_id: int = None) -> TypeGuard[Union[str, None]]:  # noqa: E501
     """
     Creates an invite link for the specified activity.
 
